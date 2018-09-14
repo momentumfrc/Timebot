a5c5b6514c68382fbb6c5ea49efec74d1001d21a
Jordan Powers <jordanpowers1227@gmail.com>
2018-09-13 20:42:29 -0700

    Timebot respects the user's timezone

--- command.php
+++ command.php
@@ -19,7 +19,12 @@ function querySlack($url,$postdata) {
 }
 function writeToLog($string, $log) {
 	file_put_contents($log.".log", date("d-m-Y_h:i:s")."-- ".$string."\r\n", FILE_APPEND);
+
 }
+function getDateString() {
+  return "<!date^".date('U')."^{time}|".date('g:i A').">";
+}
+
 date_default_timezone_set('America/Los_Angeles');
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   if($_POST["token"] == $token) {
@@ -27,8 +32,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $responded = false;
     if(file_exists("./lastTime.txt")){
       $lastDate = file_get_contents("./lastTime.txt");
-      if($currentDate - $lastDate > 2629746000) {
-        $response = array("response_type" => "in_channel","text" => "It's ".date("h:i")."\n\n_It's been a long, long time. How have you been?.._");
+      if($currentDate - $lastDate > 2592000) {
+        $response = array("response_type" => "in_channel","text" => "It's ".getDateString()."\n\n_It's been a long, long time. How have you been?.._");
         echo(json_encode($response));
         $responded = true;
       }
@@ -37,14 +42,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     if($responded) {
       exit();
     }
-    $rand = rand(0,5);
-    $time = date("g:i");
-    $response = array("response_type"=> "in_channel", "text" => "It's ".$time);
+    $rand = rand(0,4);
+    $time = date("g:i a");
+    $response = array("response_type"=> "in_channel", "text" => "It's ".getDateString());
     switch($time) {
-        case "4:20":
+        case "4:20 pm":
             if($rand == 0) {
                 $response["text"] .= "\nayyyy";
             }
+            break;
+        case "10:00 pm":
+            $response["text"] .= "\n_Goodnight Andrew_";
             break;
     }
     echo(json_encode($response));
