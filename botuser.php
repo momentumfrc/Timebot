b362a93b73ac8a0db428167fe9e195686277a29b
Jordan Powers <jordanpowers1227@gmail.com>
2018-11-14 15:25:37 -0800

    Started working on TimeBucks

--- botuser.php
+++ botuser.php
@@ -1,100 +1,9 @@
 <?php
-include 'specificvars.php';
+include 'functions.php';
 
-date_default_timezone_set("America/Los_Angeles");
-
-/**
- * Preform a POST request on a specified url with the specified parameters
-* @param string $url The url to query
-* @param array $opts The url paramteters
-* @return string The server's response
-*/
-function post_query_slack($url,$opts) {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($opts));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($curl);
-    curl_close($curl);
-    return $data;
-}
-function postMessage($channel, $message) {
-    global $bot_token;
-    post_query_slack("https://slack.com/api/chat.postMessage",array("token"=>$bot_token,"channel"=>$channel,"text"=>$message));
-}
-function writeToLog($string, $log) {
-	file_put_contents("./".$log.".log", date("d-m-Y_h:i:s")."-- ".$string."\r\n", FILE_APPEND);
-}
-function stopTimeout() {
-    ignore_user_abort(true);
-    ob_start();
-    header('Connection: close');
-    header('Content-Length: '.ob_get_length());
-    http_response_code(200);
-    ob_end_flush();
-    flush();
-}
-function verifySlack() {
-    global $slack_signing_secret;
-    $headers = getallheaders();
-    if(! (isset($headers['X-Slack-Request-Timestamp']) && isset($headers['X-Slack-Signature']))) {
-      die("Invalid headers");
-    }
-    if(abs(time() - $headers['X-Slack-Request-Timestamp']) > 60 * 5) {
-      die("Request too old");
-    }
-    $signature = 'v0:' . $headers['X-Slack-Request-Timestamp'] . ":" . file_get_contents('php://input');
-    $signature_hashed = 'v0=' . hash_hmac('sha256', $signature, $slack_signing_secret);
-    return hash_equals($signature_hashed, $headers['X-Slack-Signature']);
-  }
-function getDateString($timestamp) {
-    return "<!date^".$timestamp."^{time}|".date('g:i A', $timestamp).">";
-}
-
-$addendums = array(
-    "_Can't you just use a clock?_",
-    "_Why is this my purpose in life?_",
-    "_Here I am, brain the size of a planet and they ask me to tell the time. Call that job satisfaction? 'Cos I don't._",
-    "_Was that really easier than just looking at a clock?_",
-    "_What mind-numbingly dull task shall I perform next?_"
-);
-
-function normalResponse($event) {
-    global $addendums;
-    $response = "It's ".getDateString(floor($event["ts"]));
-    $time = date("g:i a", floor($event["ts"]));
-    switch($time) {
-        case "4:20 am":
-            $response .= "\nayyyy";
-            $response .= "\n_It's too early for this shit_";
-            break;
-        case "4:20 pm":
-            $date = (int)date("dmY");
-            srand($date);
-            $val = rand(0,4);
-            if($val == 1) {
-                $response .= "\nayyy";
-            } elseif ($val == 2) {
-                $response = "no";
-            } elseif ($val == 3) {
-                $response = 'Please pay $0.99 to unlock this dank meme';
-            }
-            break;
-        case "10:00 pm":
-            $response .= "\n_Goodnight Andrew_";
-            break;
-        case "11:11 pm":
-            $response .= "\n_Make a wish!_";
-            break;
-        default:
-            if(rand(0,4) == 1) {
-                $response .= "\n".$addendums[array_rand($addendums)];
-            }
-            break;
-    }
-    postMessage($event["channel"],$response);
-}
+$TIMEBUCKS_INCREMENT = 1;
+$TIME_COST = 1;
+$MEME_COST = 2;
 
 if($_SERVER["REQUEST_METHOD"] == "POST" && verifySlack()) {
     $headers = getallheaders();
@@ -113,16 +22,72 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && verifySlack()) {
             $event = $data["event"];
             switch($event["type"]) {
                 case "app_mention":
-                    # Moved all the logic to the normalResponse() method so that timebot can respond to phrases other than just '@timebot'
-                    normalResponse($event);
+                    $DB = createDBObject();
+                    checkUserInDB($DB, $event["user"]);
+                    $userinfo = getUserInfo($DB,$event["user"]);
+
+                    $response = "It's ".getDateString(floor($event["ts"]));
+                    $time = date("g:i a", floor($event["ts"]));
+
+                    $cost = $TIME_COST;
+                    switch($time) {
+                        case "4:20 am":
+                            $cost = $MEME_COST;
+                            $response .= "\nayyyy";
+                            $response .= "\n_It's too early for this shit_";
+                            break;
+                        case "4:20 pm":
+                            $cost = $MEME_COST;
+                            $response .= "\nayyy";
+                            break;
+                        case "10:00 pm":
+                            $cost = $MEME_COST;
+                            $response .= "\n_Goodnight Andrew_";
+                            break;
+                    }
+                    $timebucks = $userinfo["balance"] - $cost;
+                    if($timebucks < 0) {
+                        postMessage($event["channel"],$userinfo["id"],"I'm sorry <@".$userinfo["id"].">, but you have insufficient TimeBucks!\n This action costs $".number_format($cost,2)."\nYour current balance is $".number_format($userinfo["balance"],2));
+                    } else {
+                        updateUserBalance($DB,$userinfo["id"],$timebucks);
+                        postMessage($event["channel"],$response);
+                    }
                     break;
-                case "message":
-                    #writeToLog("Message of type ".$event["channel_type"]." from ".$event["user"],"events");
-                    if($event["user"] == "") {
+                case "message";
+                    # Prevent timebot from responding to itself
+                    if($event["subtype"] == "bot_message" || $event["user"] == "") {
                         exit();
                     }
+
                     switch($event["channel_type"]) {
                         case "channel":
+                            $text = strtolower($event["text"]);
+                            $matches = array();
+        
+                            $valid_time = false;
+        
+                            if(preg_match("/it\'?s 0?((1?[1-9]|2[0-4])\:[0-5][0-9])( |\$)/",$text,$matches)) {
+                                # 24 hour time
+                                $supposed_time = $matches[1];
+                                $current_time = date("G:i", floor($event["ts"]));
+                                $valid_time = $supposed_time == $current_time;
+                            } elseif(preg_match("/it\'?s 0?(([1-9]|1[0-2])\:[0-5][0-9]) ?(am|pm)/",$text,$matches)) {
+                                # 12 hour time
+                                $supposed_time = $matches[1].$matches[3];
+                                $current_time = date("g:ia");
+                                $valid_time = $supposed_time == $current_time;
+                            }
+        
+                            if($valid_time) {
+                                $DB = createDBObject();
+                                checkUserInDB($DB, $event["user"]);
+                                $userinfo = getUserInfo($DB,$event["user"]);
+                                $timebucks = $userinfo["balance"] + $TIMEBUCKS_INCREMENT;
+                                updateUserBalance($DB, $userinfo["id"],$timebucks);
+                                $message = "Your TimeBucks balance is now $".number_format($timebucks,2);
+                                postEphemeral($event["channel"],$userinfo["id"],$message);
+                            }
+                            
                             # Respond to an 'ayyy' in #random
                             if(stripos($event["text"], "ayyy") !== false) {
                                 $time = date("g:i a", floor($event["ts"]));
@@ -130,10 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && verifySlack()) {
                                     postMessage($event["channel"], "F");
                                 }
                             }
-                            # Respond to 'Timebot, ACTIVATE' in the same way as '@timebot'
-                            if(strpos($event["text"], "Timebot, ACTIVATE") !== false) {
-                                normalResponse($event);
-                            }
+
                             if(date("m-d") == "06-19" && strpos($event["text"], "Happy Birthday Timebot!")) {
                                 sleep(1);
                                 postMessage($event["channel"], "Why thank you!");
@@ -141,9 +103,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && verifySlack()) {
                             break;
                         case "im":
                             # Respond to DMs
-                            $response = "It's ".getDateString(floor($event["ts"]));
-                            $response .= "\n".$addendums[array_rand($addendums)];
-                            postMessage($event["channel"], $response);
+                            $DB = createDBObject();
+                            checkUserInDB($DB, $event["user"]);
+                            $userinfo = getUserInfo($DB,$event["user"]);
+                            postMessage($event["channel"],"Your TimeBucks balance is $".number_format($userinfo["balance"],2));
                             break;
                     }
                     break;
---
+++ functions.php
@@ -0,0 +1,159 @@
+<?php
+include 'specificvars.php';
+
+date_default_timezone_set("America/Los_Angeles");
+
+/**
+ * Write a message to a log. The log is in the same directory as the current php file and has the name $log with the extension .log
+ * @param string $string The message to write
+ * @param string $log The name of the log to write to
+ */
+function writeToLog($string, $log) {
+	file_put_contents("./".$log.".log", date("d-m-Y_h:i:s")."-- ".$string."\r\n", FILE_APPEND);
+}
+/**
+ * Preform a POST request on a specified url with the specified parameters
+ * @param string $url The url to query
+ * @param array $opts The url parameters
+ * @return string The server's response
+ */
+function post_query_slack($url,$opts) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($opts));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
+/**
+* Preform a GET request on a specified url with the specified parameters
+* @param string $url The url to query
+* @param array $opts The url parameters
+* @return string The server's response
+*/
+function get_query_slack($url,$opts) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url.'?'.http_build_query($opts));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+    curl_close($curl);
+    return $data;
+}
+/**
+ * Post a message to slack
+ * @param string $channel The channel to post to
+ * @param string $message The message text to send
+ */
+function postMessage($channel, $message) {
+    global $bot_token;
+    post_query_slack("https://slack.com/api/chat.postMessage",array("token"=>$bot_token,"channel"=>$channel,"text"=>$message));
+}
+function postEphemeral($channel, $user, $text) {
+    global $bot_token;
+    $options = array(
+        "token"=>$bot_token,
+        "channel"=>$channel,
+        "text"=>$text,
+        "as_user"=>false
+    );
+    post_query_slack("https://slack.com/api/chat.postEphemeral",$optoins);
+}
+/**
+ * Prevent slack from thinking the server timed out
+ */
+function stopTimeout() {
+    ignore_user_abort(true);
+    ob_start();
+    header('Connection: close');
+    header('Content-Length: '.ob_get_length());
+    http_response_code(200);
+    ob_end_flush();
+    flush();
+}
+/**
+ * Make sure a recieved request is from slack
+ * @return boolean If the request is verified to be from slack
+ */
+function verifySlack() {
+    global $slack_signing_secret;
+    $headers = getallheaders();
+    if(! (isset($headers['X-Slack-Request-Timestamp']) && isset($headers['X-Slack-Signature']))) {
+      die("Invalid headers");
+    }
+    if(abs(time() - $headers['X-Slack-Request-Timestamp']) > 60 * 5) {
+      die("Request too old");
+    }
+    $signature = 'v0:' . $headers['X-Slack-Request-Timestamp'] . ":" . file_get_contents('php://input');
+    $signature_hashed = 'v0=' . hash_hmac('sha256', $signature, $slack_signing_secret);
+    return hash_equals($signature_hashed, $headers['X-Slack-Signature']);
+}
+/**
+ * Format a timestamp as a string that slack will interpret and display in the user's timezone
+ * @param int The timestamp to format
+ * @return string The formatted string
+ */
+function getDateString($timestamp) {
+    return "<!date^".$timestamp."^{time}|".date('g:i A', $timestamp).">";
+}
+/**
+ * Get a mysqli database object for the users database
+ * @return mixed The mysqli object representing the database connection, or FALSE if there was an error connecting
+ */
+function createDBObject() {
+    global $dbuser, $dbpass, $database;
+    $DB = new mysqli("localhost", $dbuser, $dbpass, $database);
+    if($DB->connect_error) {
+        return FALSE;
+    } else {
+        return $DB;
+    }
+}
+function checkUserInDB($DB, $user) {
+    global $table;
+    $stmt = $DB->prepare("INSERT INTO ".$table." (`id`) VALUES (?) ON DUPLICATE KEY UPDATE `id`=VALUES(`id`)");
+    if($stmt === FALSE) {
+        throw new Exception($DB->error);
+    }
+    $stmt->bind_param("s",$user);
+    $stmt->execute();
+    $stmt->close();
+}
+/**
+ * Get the information stored for a user
+ * @param mysqli $DB The database connection
+ * @param string $user The userid for the user whose information is to be returned
+ * @return array An associative array containing the user id and timebucks balance of the user
+ */
+function getUserInfo($DB, $user) {
+    global $table;
+    $stmt = $DB->prepare("SELECT * FROM ".$table." WHERE `id`=?");
+    if($stmt === FALSE) {
+        throw new Exception($DB->error);
+    }
+    $stmt->bind_param("s",$user);
+    $stmt->execute();
+    $stmt->bind_result($id, $balance);
+    $stmt->fetch();
+    $stmt->close();
+    return array("id"=>$id,"balance"=>$balance);
+}
+/**
+ * Set the user's timebucks balance
+ * @param mysqli $DB The database connection
+ * @param string $user The userid for the user whose timebucks balance is to be updated
+ * @param double $newbalance The new timebucks balance for the user
+ */
+function updateUserBalance($DB, $user, $newbalance) {
+    global $table;
+    $stmt = $DB->prepare("UPDATE ".$table." SET `balance`=? WHERE `id`=?");
+    if($stmt === FALSE) {
+        throw new Exception($DB->error);
+    }
+    $stmt->bind_param("ds",$newbalance,$user);
+    $stmt->execute();
+    $stmt->close();
+}
+
+?>
